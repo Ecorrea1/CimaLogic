6a8d1d0cf512f15c4366de57ca024662c6dbf93d
Emmanuel Correa <ebcorreac@gmail.com>
2023-12-11 23:58:32 -0300

    [ADD] Login

---
+++ js/login.js
@@ -0,0 +1,96 @@
+"use strict";
+
+let emailValidator = false;
+let passValidator = false;
+
+const form = document.getElementById('form-sesion');
+const inputEmail = document.getElementById('input-email');
+const inputPass = document.getElementById('input-pass');
+
+const labelErrorEmail = document.getElementById('divErrorEmail');
+const labelErrorPass = document.getElementById('divErrorPass');
+
+const checkRememberMe = document.getElementById('remember-me');
+const btnAccess = document.getElementById('btn-access');
+
+// const alertMessage = document.getElementById('alert-msg');
+
+
+function showAlert(message) {
+
+    let element = document.getElementById("alerts");
+    element.classList.remove("d-none");
+    element.innerHTML = message;
+
+}
+function clearForm(){
+    inputEmail.value= "";
+    inputPass.value="";
+}
+
+const sendSession = async ( data) => {  
+    return await fetch( api + 'auth' , {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(data)
+    })
+    .then((data)=> data.json())
+    .then(response => {
+        console.log(response.ok);
+        const { email, name, role } = response.data;
+        console.log(response);
+        
+        // localStorage.setItem("token", response.token);
+        localStorage.setItem("email", email);
+        localStorage.setItem("name", name);
+        localStorage.setItem("role", role);
+        return true;
+      }
+    )
+    .catch(err => {
+       console.error(err);
+      return false;
+    });
+  }
+
+async function sendInfo(){
+    // emailValidator = validateAllfields(inputEmail, labelErrorEmail);
+    // passValidator = validateAllfields(inputPass, labelErrorPass);
+    
+    const data = ({
+        "email": inputEmail.value,
+        "pass": inputPass.value
+    });
+    
+   const result =  await sendSession(data)
+    if(!result) return showAlert('Hay problemas para iniciar sesion');
+    showAlert('iniciando sistemas');
+   
+    location.replace( url + '/index.html');
+}
+
+btnAccess.addEventListener('click', async (e) => {
+    e.preventDefault();
+    // if(inputEmail.value === '' || inputPass.value === '') return inputEmail.value = 'Ingresa algo'
+
+    //Validación de correo electrónico
+    // let regexEmail = /^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+    // emailValidator = (regexEmail.test(inputEmail.value)) ? true : false;
+    // labelErrorEmail.innerHTML =  emailValidator ? '' : 'Correo inválido';
+  
+    //Validación de contraseña
+    // passValidator = (inputPass.value.length >= 8) ? true : false;
+    // labelErrorPass.innerHTML = passValidator ? '' : 'Contraseña demasiado corta';
+
+    await sendInfo();
+
+})
+
+//Verificar si el usuario ya ha iniciado sesión
+
+window.addEventListener("load", async() => {
+
+//    const isLogged = await getDataUser();  //Verificar si el usuario ya ha iniciado sesión
+//    if(isLogged) return window.location.href = "index.html"
+    clearForm()
+})
